6e264064c7985bd7aede7838a034a7fb225bf468
Daniele Venafro <danielevenafro@gmail.com>
2025-04-02 23:37:19 +0200

    Added HUD interface

---
+++ Assets/Script/HUD/HealthBArControl.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class HealthBArControl : MonoBehaviour
+{
+    private UIDocument _uiDocument;
+    void Start()
+    {
+        _uiDocument = GetComponent<UIDocument>();
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
